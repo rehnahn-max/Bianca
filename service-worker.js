e3dfb8ccc97e2635127210ec8950ae953f2a7ace
rehnahn-max <rehnahn@gmail.com>
2026-08-20 18:20:50 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,79 +1,71 @@
-const CACHE_NAME = 'bia-nails-v1';
+const CACHE_NAME = "bia-nails-v2";
 
-const urlsToCache = [
-'./',
-'./index.html',
-'./wallpaperbackspace.png'
+const FILES = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
-});
+self.addEventListener("install", event => {
 
-self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-);
-});
+  self.skipWaiting();
 
-const portfolio = document.querySelectorAll(".portfolio-slide");
-
-let foto = 0;
-
-setInterval(()=>{
-
-    portfolio[foto].classList.remove("active");
-
-    foto++;
-
-    if(foto >= portfolio.length){
-
-        foto = 0;
-
-    }
-
-    portfolio[foto].classList.add("active");
-
-},4000);
-
-const imagens = document.querySelectorAll(".carrossel img");
-const pontos = document.querySelectorAll(".bolinhas span");
-
-let atual = 0;
-
-function trocar(){
-
-    imagens[atual].classList.remove("ativo");
-    pontos[atual].classList.remove("ativa");
-
-    atual++;
-
-    if(atual >= imagens.length){
-
-        atual = 0;
-
-    }
-
-    imagens[atual].classList.add("ativo");
-    pontos[atual].classList.add("ativa");
-
-}
-
-setInterval(trocar,4000);
-
-
-navigator.serviceWorker.getRegistrations().then(registrations => {
-
-    registrations.forEach(reg => reg.update());
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
 
 });
 
-navigator.serviceWorker.addEventListener("controllerchange", () => {
+self.addEventListener("activate", event => {
 
-    window.location.reload();
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, clone));
+
+        return response;
+
+      })
+
+      .catch(() =>
+        caches.match(event.request)
+      )
+
+  );
 
 });
